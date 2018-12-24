8c110fc262e8e1fa1db06a8fe1ed038a7f77bbd8
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2018-12-24 20:16:18 -0200

    Comparation operators

---
+++ exemplo-09.php
@@ -0,0 +1,20 @@
+<?php
+
+$a = 55.0;
+$b = 55;
+
+var_dump($a > $b);
+echo "<br/>";
+var_dump($a < $b);
+echo "<br/>";
+//var_dump($a = $b); ---> Nunca podemos fazer isto, pois estamos atribuindo no var_dump();
+echo "<br/>";
+var_dump($a == $b);//Compara só o valor
+echo "<br/>";
+var_dump($a === $b);//Compara valor e tipo
+echo "<br/>";
+var_dump($a != $b);
+echo "<br/>";
+var_dump($a !== $b);
+
+?>
