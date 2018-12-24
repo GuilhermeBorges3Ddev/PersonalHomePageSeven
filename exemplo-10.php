dc83564966228287847f54122440b7af2ab49a61
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2018-12-24 20:31:32 -0200

    Operator spaceship and Null Coalesce from PHP7

---
+++ exemplo-10.php
@@ -0,0 +1,14 @@
+<?php
+//Operador SpaceShip, ligado à versões PHP >= 7.0.0
+$a = 50;
+$b = 35;
+var_dump($a <=> $b);//Se "a" for maior traz 1, se for igual traz 0, se a for menor traz -1
+echo "<br/>";
+
+//Operador Null Coalesce, ligado à versões PHP >= 7.0.0
+$c = NULL;
+$d = NULL;
+$e = 10;
+echo $c ?? $d ?? $e;//Leia "c" se nao for nulo, se for leia "d", se ele também for leia "e"
+//OBS: Ele para no primeiro não nulo, se todos forem nulos ele não retorna nada
+?>
