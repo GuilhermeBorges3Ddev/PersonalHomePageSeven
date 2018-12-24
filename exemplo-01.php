5a1a9a8c31a34aca00a3610da95ba02bbcec8620
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2018-12-24 13:47:36 -0200

    Adding a simple example using vars and the function var_dump

---
+++ exemplo-01.php
@@ -0,0 +1,13 @@
+<?php 
+
+$nome = "Hcode";
+$separador = "---------"; 
+/*Apesar de fracamente tipada PHP aceita tipar, exemplo:*/
+String $name = "Gcode";
+
+
+echo $nome; // Retorna apenas o tipo
+echo $separador;
+var_dump($nome);//Retorna o tipo, o tamanho e o valor
+
+?>
