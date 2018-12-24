f737c279cbf4539988166c7d86fbf30114d8db95
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2018-12-24 19:44:30 -0200

    Assignment combinning increase and decrease

---
+++ exemplo-07.php
@@ -0,0 +1,13 @@
+<?php
+//Operador de atribuição incrementando e decrementando int ou float
+$valorTotal = 0;
+$valorTotal += 100;
+$valorTotal += 25;
+$valorTotal -= 10;
+echo $valorTotal;
+echo "<br/>";
+
+$valorTotal *= .9;//Simulando um desconto de 10%
+
+echo $valorTotal;
+?>
