b2ec11226c7a80f15174ee45975a2151e38c298f
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2018-12-24 19:53:49 -0200

    Example of arithmetic operators

---
+++ exemplo-08.php
@@ -0,0 +1,17 @@
+<?php
+//Operadores aritméticos
+$a = 10;
+$b = 2;
+echo $a + $b;
+echo "<br/>";
+echo $a - $b;
+echo "<br/>";
+echo $a * $b;
+echo "<br/>";
+echo $a / $b;
+echo "<br/>";
+echo $a % $b;
+echo "<br/>";
+echo $a ** $b;//Isto é o mesmo que "a" elevado à "b"
+echo "<br/>";
+?>
