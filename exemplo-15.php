cdacbe97f7ed63f8cf7de26440acfc6809cba5d8
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2018-12-25 02:03:50 -0200

    Funtions str_replace(), substr(), strpos()

---
+++ exemplo-15.php
@@ -0,0 +1,24 @@
+<?php
+
+$empresa = "Hcode";//Vamos trocar o "o" pelo "0" e o "e" pelo "3"
+$empresa = str_replace("o", "0", $empresa);
+$empresa = str_replace("e", "3", $empresa);
+echo $empresa;
+echo "<br />";
+
+//Usando strpos() para procur
+$frase = "A repetição é a mãe da retenção.";
+$palavra = "mãe";
+$q = strpos($frase, "mãe");//Sintaxe alvo,busca ---> O nome $q é a sintaxe do google para query
+var_dump($q);
+echo "<br />";
+
+//Pega todo texto até o resultado de $q, que é "mãe" na posição 17
+$texto = substr($frase, 0, $q);
+var_dump($texto);
+echo "<br />";
+
+//Pega o texto de 17 para frente
+$texto2 = substr($frase, $q + strlen($palavra), strlen($frase));
+var_dump($texto2);
+?>
