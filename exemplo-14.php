e153612245d4dc238d770b8a16ebff7d7c5d390b
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2018-12-25 01:23:05 -0200

    Using functions strtolower, strtoupper, ucwords and ucfirst

---
+++ exemplo-14.php
@@ -0,0 +1,20 @@
+<?php
+
+$nome = "joao silva";
+$nome = strtoupper($nome);//Função que recebe uma String e o converte em uma maiúscula
+echo $nome;
+echo "<br />";
+
+$nome = strtolower($nome);//Função que recebe uma String e o converte em uma minúscula
+echo $nome;
+echo "<br />";
+
+//Vamos agora mudar a primeira letra de cada palavra para maiúscula
+echo ucwords($nome);
+echo "<br />";
+
+//Mudando apenas a primeira letra da primeira palavra
+echo ucfirst($nome);
+echo "<br />";
+
+?>
