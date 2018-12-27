bd4194eb98df2e1be46963f99d7cf252c62a9781
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2018-12-27 18:33:33 -0200

    Creating a simple constant by function define() and a constant array using the same function with three parameters

---
+++ exemplo-28.php
@@ -0,0 +1,18 @@
+<?php
+
+define("SERVIDOR", "127.0.0.1");//Função define() usa como primeiro parâmetro o nome seguido do valor
+
+echo SERVIDOR;//Constantes não precisam do dollar '$' 
+echo "<br><br/>";
+
+//Definindo um Array dentro de uma constante, novidade das versões PHP >= 7.0.0
+define("BANCO_DE_DADOS", [
+	'127.0.0.0',
+	'root',
+	'password123',
+	'textBD'
+],true);//Com o 3º parâmetro 'true' nós tornamos a constante Case Insensitive
+
+print_r(BANCO_DE_DADOS);
+
+?>
