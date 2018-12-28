88687a70fc1811b61daffc29e6d16863bbf8d835
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2018-12-28 00:45:59 -0200

    Creatting user functions with return and without return, after you can see a mixture between a simple echo containning a return together

---
+++ exemplo-34.php
@@ -0,0 +1,27 @@
+<?php
+
+function ola(){
+
+	//echo "Bem Vindo Usuário!<br />";
+	return "Bem Vindo Usuário!";
+}
+
+//Chamada de função
+//ola();
+echo ola();
+echo "<br><br />";
+
+//Colocando o conteúdo do retorno em uma variável
+$frase  = ola();
+
+//Verificando o tamanho da frase retornada ao usuário
+echo "Esta mensagem acima possui: ";
+echo strlen($frase) . " caracteres, contando os espaços em branco <br><br />";
+
+//Usando a declaração da função com sintaxe mínima
+function salario(){ return 946.00;}
+echo "Maria recebeu um decimo terceiro salario de: " . (salario()*3);
+
+
+
+?>
