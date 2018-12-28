e97683eb49c44c56588b16f2d94d1da059caa75e
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2018-12-27 22:58:46 -0200

    These example includes the file config.php and creates a single session variable

---
+++ exemplo-30.php
@@ -0,0 +1,9 @@
+<?php
+
+//Chamando arquivo de marcação para sessão ativa nesta página
+require_once("config.php");
+
+//Criação de uma única variável de seção para testes
+$_SESSION["nome"] = "Hcode";
+
+?>
