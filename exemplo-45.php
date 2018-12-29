3777713f10281ed6d928db1bdb32151006fc7178
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2018-12-28 23:49:33 -0200

    Introducing the OO concept in PHP, cratting the class and the respective instance, who access and modify the value of an attributo and shows a message printed on method

---
+++ exemplo-45.php
@@ -0,0 +1,25 @@
+<?php
+
+class Pessoa {
+
+	//Atributo
+	public $nome;
+
+	//Método
+	public function falar(){
+
+		return "O meu nome é " . $this->nome;
+
+	}
+
+}
+
+$glaucio = new Pessoa();
+
+//Nós só acessamos e alteramos porque o atributo é público
+$glaucio->nome = "Glaucio Daniel Saurom";
+
+//O mesmo vale para o método, que também tem acesso público
+echo $glaucio->falar();
+
+?>
