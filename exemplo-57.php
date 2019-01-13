107e7925f5f8caa0fc364ecf904006618cc58bd5
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2019-01-13 00:01:26 -0200

    Connecting with the workbench using the class 'mysqli', these code insert two users in the table tb_usuarios

---
+++ exemplo-57.php
@@ -0,0 +1,33 @@
+<?php
+
+//IP -> Usuário -> Senha -> BD
+$conn = new mysqli("localhost","root","","dbphp7");
+
+//Condicional para verificação de erro no conexão
+if ($conn->connect_error){
+
+	echo "Error: " . $conn->connect_error;//Retorno vazio = sem erros!!
+	exit;
+}
+
+//O método prepare() prepara o banco para o recebimento de comandos DDL/DML
+$stmt = $conn->prepare("INSERT INTO tb_usuarios (deslogin,dessenha) VALUES (?,?)");
+
+//Esse método diz ao banco quais são os tipos de parâmetros que são esperados pelas interrogações
+$stmt->bind_param("ss",$login,$pass);//ss significa duas Strings: user e 12345
+
+//Variáveis passadas como referência ao método bind_param()
+$login = "user";
+$pass = "12345";
+
+//Faz o envio dos comandos dados para o MySQL Workbench
+$stmt->execute();
+
+//Adicionando um segundo usuário na tabela tb_usuarios
+$login = "root";
+$pass = "!@#$";
+
+//Executando no banco a segunda inserção
+$stmt->execute();
+
+?>
