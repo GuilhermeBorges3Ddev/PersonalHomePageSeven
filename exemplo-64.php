c5bc37d30e095f0ab744799335684de728da8bf8
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2019-01-15 02:09:01 -0200

    Checking a deletion of a data using PDO

---
+++ exemplo-64.php
@@ -0,0 +1,17 @@
+<?php
+
+//Nome do banco -> Database -> Local/Nome da Instância -> Estado de Conexão (0/1.mono/multithread) -> Login -> Senha
+$conn = new PDO("sqlsrv:Database=dbphp7;server=localhost\SQLEXPRESS;ConnectionPooling=0", "sa", "root");
+
+//Execução da exclusão com a notação de id
+$stmt = $conn->prepare("DELETE FROM tb_usuarios WHERE idusuario = :ID");
+$id = 1;
+
+//Deleta os valores do usuário com o ID igual a '1' 
+$stmt->bindParam(":ID",$id);
+
+$stmt->execute();
+
+echo "Dados deletados para o usuário com ID igual a 1!";
+
+?>
