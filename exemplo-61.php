dc32ce40d6aa2cfb7a3f6169ba49f9b0360fe196
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2019-01-15 00:40:57 -0200

    PDO over the SQL Server Express in the file exemplo-61.php and a query exemplo-61.sql what tests our connection

---
+++ exemplo-61.php
@@ -0,0 +1,14 @@
+<?php
+
+//Nome do banco -> Database -> Local/Nome da Instância -> Estado de Conexão (0/1.mono/multithread) -> Login -> Senha
+$conn = new PDO("sqlsrv:Database=dbphp7;server=localhost\SQLEXPRESS;ConnectionPooling=0", "sa", "root");
+
+$stmt = $conn->prepare("SELECT * FROM tb_usuarios ORDER BY deslogin;");
+
+$stmt->execute();
+
+$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+echo json_encode($results);
+
+?>
---
+++ exemplo-61.sql
@@ -0,0 +1,3 @@
+SELECT * FROM tb_usuarios;
+
+INSERT INTO tb_usuarios(deslogin,dessenha)VALUES('localhost','a1b2c3');
