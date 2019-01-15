15dd93ca078f079500e2c811b78a57e6b18db2d2
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2019-01-15 02:50:13 -0200

    Applying the transaction concept starting with method beginTransaction() and submiting the operation with another method, the commit()

---
+++ exemplo-65.php
@@ -0,0 +1,22 @@
+<?php
+
+//Nome do banco -> Database -> Local/Nome da Instância -> Estado de Conexão (0/1.mono/multithread) -> Login -> Senha
+$conn = new PDO("sqlsrv:Database=dbphp7;server=localhost\SQLEXPRESS;ConnectionPooling=0", "sa", "root");
+
+//Marcador de início de uma transação
+$conn->beginTransaction();
+
+//Execução da exclusão com a notação de id, só que ao invés de :ID usaremos a '?'
+$stmt = $conn->prepare("DELETE FROM tb_usuarios WHERE idusuario = ?");
+$id = 2;
+
+//Caso houvesse mais de um parâmetro marcado com '?' teríamos que passá-lo na ordem em que foram usados
+$stmt->execute(array($id));
+
+//No caso do rollback() ele fará a operação mas não apaga o banco com a deleção criada
+//$conn->rollback();
+$conn->commit();
+
+echo "Transacao efetuada com sucesso!";
+
+?>
