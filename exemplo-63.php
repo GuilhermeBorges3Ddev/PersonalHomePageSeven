c9e297820e302e61190a939935b094a73793e6af
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2019-01-15 01:58:24 -0200

    Using a query with UPDATE, making again ID's to give security on the PDO used

--- exemplo-63.php
+++ exemplo-63.php
@@ -3,16 +3,16 @@
 //Nome do banco -> Database -> Local/Nome da Instância -> Estado de Conexão (0/1.mono/multithread) -> Login -> Senha
 $conn = new PDO("sqlsrv:Database=dbphp7;server=localhost\SQLEXPRESS;ConnectionPooling=0", "sa", "root");
 
-//Execução da inserção com a notação de id
+//Execução da atualização com a notação de id
 $stmt = $conn->prepare("UPDATE  tb_usuarios SET deslogin = :LOGIN, dessenha = :PASSWORD WHERE idusuario = :ID");
 $login = "joao";
 $password = "qwerty";
 $id = 2;
 
-//Atribui valores aos nossos id's passados na função prepare()
+//Modfica valores dos nossos id's passados na função prepare()
 $stmt->bindParam(":LOGIN",$login);
 $stmt->bindParam(":PASSWORD",$password);
-$stmt->bindParam(":ID",$password);
+$stmt->bindParam(":ID",$id);
 
 $stmt->execute();
 
