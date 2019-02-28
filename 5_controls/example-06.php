43b05088cc7a20124e853c3ce27adc96a41452d7
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2019-02-28 18:06:12 -0300

    Foreach applyed to make a $_GET in the <form> tag

--- 5_controls/example-06.php
+++ 5_controls/example-06.php
@@ -1,3 +1,6 @@
+	
+	<!-- $_GET combinado com foreach para pegarmos cada valor e nome do campo no <form>-->
+
 	<form><!-- Como não tem padrão de 'action' para o form o padrão será GET-->
 
 		<input type="text" name="nome">
@@ -9,13 +12,13 @@
 	<?php
 
 	if(isset($_GET)){
-		//$jey irá pegar o 'name' do devido input
+		//$key irá pegar o 'name' do devido input
 		//$value irá pegar o 'valor' preenchido no input
 		foreach ($_GET as $key => $value) {
 		
 			echo "Nome do campo: " .$key."<br />" ;
 			echo "Valor do campo: " .$value."<br />" ;
-			echo "<hr>";//Usamos como separador aqui uma linha horizontal
+			echo "<hr>";
 		}
 	}
 
