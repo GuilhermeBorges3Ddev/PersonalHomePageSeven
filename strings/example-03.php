3e851a6aa8fe11ac8035ca5499dcfcacaf5ee85d
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2019-02-28 15:25:37 -0300

    Third folder called 'strings' was created and finished

--- strings/example-03.php
+++ strings/example-03.php
@@ -1,12 +1,14 @@
 <?php
 
+//Funções para Strings Php que realizam troca, busca e retirada de partes da String
+
 $empresa = "Hcode";//Vamos trocar o "o" pelo "0" e o "e" pelo "3"
 $empresa = str_replace("o", "0", $empresa);
 $empresa = str_replace("e", "3", $empresa);
 echo $empresa;
 echo "<br />";
 
-//Usando strpos() para procur
+//Usando strpos() para procura de palavra retornando a posição na String
 $frase = "A repetição é a mãe da retenção.";
 $palavra = "mãe";
 $q = strpos($frase, "mãe");//Sintaxe alvo,busca ---> O nome $q é a sintaxe do google para query
