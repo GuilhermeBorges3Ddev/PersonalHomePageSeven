51af89ff8676d9e9af7790b0d53d51395e563518
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2019-02-28 15:52:02 -0300

    Foldes 'includes' done and example-02 are calling example-01

---
+++ 4_includes/example-02.php
@@ -0,0 +1,12 @@
+<?php
+
+include "example-01.php";//Incluindo arquivos do mesmo diretório
+//require "example-01.php"; --> Incluindo outro arquivo via require
+//require_once "example-01.php"; --> Só traz o arquivo se ele não tiver sido chamado anteriormente
+
+
+$resultado = somar(10,20);
+echo "<br />";
+echo '------------------------------- <br />';
+echo $resultado;
+?>
