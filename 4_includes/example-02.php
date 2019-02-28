54ee43ebe97950c8b5b15063a0d27faf2344480e
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2019-02-28 16:08:46 -0300

    includesFolder done

--- 4_includes/example-02.php
+++ 4_includes/example-02.php
@@ -1,9 +1,8 @@
 <?php
 
-include "inc/example-01.php";//Incluindo arquivos do mesmo diretório
-//require "example-01.php"; --> Incluindo outro arquivo via require
-//require_once "example-01.php"; --> Só traz o arquivo se ele não tiver sido chamado anteriormente
-
+//include "inc/example-01.php"; --> Incluindo via include não temos garantia de funcionanmento, lembrar inc_path e extenal include
+//require "inc/example-01.php"; --> Incluindo via require obriga que o arquivo exista e funcione
+require_once "inc/example-01.php"; //--> Traz o arquivo se ele não for chamado anteriormente e se estiver funcionando
 
 $resultado = somar(10,20);
 echo "<br />";
