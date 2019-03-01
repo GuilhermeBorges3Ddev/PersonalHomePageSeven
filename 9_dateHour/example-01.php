406c9d49bc52b9314360c562311505f6dbf6ed10
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2019-03-01 16:16:32 -0300

    On example-01.php we can see some use of data(), time() and strtotime()

--- 9_dateHour/example-01.php
+++ 9_dateHour/example-01.php
@@ -1,10 +1,12 @@
 <?php
 
+/* Nesse exemplo temos um get de Timestamp, uso de date() e manipulação de data com timestamp() */
+
 echo time();//Ele pega a quantidade de segundos desde 01/01/1970
 echo "<br><br />";
 
 echo "Pegando a data e a hora atual (usando timestamp): <br>";
-echo date("d/m/Y H:i:s",1546031689);//Imprime dia, mês e ano; sem parâmetros ela não roda
+echo date("d/m/Y H:i:s",1546031689);//Imprime dia, mês, ano e $TIMESTAMP; sem parâmetros ela não roda
 echo "<br><br />";
 
 //Obtendo o timestamp() para o caso de usa-lo em outro lugar do código
