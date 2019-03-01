3dc6481e9593fcb24bba37d2ac634b4cb2325659
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2019-03-01 17:29:03 -0300

    example-03.php uses a single example of function dateTime(), modifyed

--- 9_dateHour/example-03.php
+++ 9_dateHour/example-03.php
@@ -1,5 +1,8 @@
 <?php
 
+/* Essa classe nativa, a DateTime() falada nesse exemplo será mutada, ou seja,
+acrescentaremos 15 dias na data corrente usando outra função, a DataInterval() */
+
 $dt = new DateTime();
 
 //Acessaremos o método 'format' da classe DateTime
@@ -9,7 +12,7 @@ echo "<br><br/>";
 
 //Usaremos a classe DateInterval, que usa 'P' para indicar período e 'D' para indicar dias
 $periodo = new DateInterval("P15D");
-$dt->add($periodo);
+$dt->add($periodo);//Para subtrair usaríamos a função oposta, a sub()
 
 echo "Retornando a data atual adicionada de 15 dias: <br/>";
 echo $dt->format("d/m/Y H:i:s");
